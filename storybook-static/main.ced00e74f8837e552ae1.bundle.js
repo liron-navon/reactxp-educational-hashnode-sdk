57e32565eb295a60c96c1f52cd6656cf7d719b7b
Liron Navon <liron.navon@clockwork.nl>
2019-03-30 15:17:22 +0300

    removed storybook-static

--- storybook-static/main.ced00e74f8837e552ae1.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{330:function(n,o,p){p(331),p(408),n.exports=p(864)},408:function(n,o,p){"use strict";p.r(o);p(409),p(769),p(782),p(861)}},[[330,1,2]]]);
